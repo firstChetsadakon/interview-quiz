7c948f3feb0c815603c76b48cff788f679d5f92b
firstChetsadakon <first.chetsadakorn@gmail.com>
2025-02-10 17:52:32 +0700

    rename WebClientConfiguration

--- src/main/java/com/ascendcorp/interviewquiz/configs/WebClientConfiguration.java
+++ src/main/java/com/ascendcorp/interviewquiz/configs/WebClientConfiguration.java
@@ -5,19 +5,25 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebClienttConfiguration {
+public class WebClientConfiguration {
 
     @Value("${bot.host}")
     private String botHost;
+
+    @Value("${bot.api-key}")
+    private String apiKey;
 
     @Bean("botWebClient")
     public WebClient webClient() {
         return WebClient
             .builder()
             .baseUrl(botHost)
+                .defaultHeader("X-IBM-Client-Id", apiKey)
+                .defaultHeader("accept", MediaType.APPLICATION_JSON_VALUE)
             .build();
     }
 
